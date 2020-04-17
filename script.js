3ff9a5fe603fd88aa83d6e446d9dedfb5047f5f5
Julia Lojek <julialojek@gmail.com>
2020-04-17 12:07:57 +0200

    function creates an object

--- script.js
+++ script.js
@@ -20,12 +20,19 @@ function getMax(...arr) {
   return Math.max(...arr);
 }
 
-console.log(calcAverage(4, 90, 27, 38));
-console.log(calcMedian(4, 90, 27, 38));
-console.log(getMin(4, 90, 27, 38));
-console.log(getMax(4, 90, 27, 38));
+console.log(calcAverage(4, 90, 27, 38) === 39.75);
+console.log(calcMedian(4, 90, 27, 38) === 32.5);
+console.log(calcMedian(4, 90, 490, 27, 38) === 38);
+console.log(getMin(4, 90, 27, 38) === 4);
+console.log(getMax(4, 90, 27, 38) === 90);
 
-console.log(calcAverage([4, 90, 27, 38]));
-console.log(calcMedian([4, 90, 27, 38]));
-console.log(getMin([4, 90, 27, 38]));
-console.log(getMax([4, 90, 27, 38]));
+function createObject(arr) {
+  return {
+    average: calcAverage(...arr),
+    median: calcMedian(...arr),
+    min: getMin(...arr),
+    max: getMax(...arr)
+  };
+}
+
+console.log(createObject([49, 274, 402, 40, 29]));
