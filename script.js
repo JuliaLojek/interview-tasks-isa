8e056abad3f9f95b3cf222dbc059924a9cdf7994
Julia Lojek <julialojek@gmail.com>
2020-04-17 12:00:17 +0200

    functions accept list of arguments

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-function calcAverage(arr) {
+function calcAverage(...arr) {
   const sum = arr.reduce((acc, value) => {
     return acc + value
   }, 0);
   return sum / arr.length;
 }
 
-function calcMedian(arr) {
+function calcMedian(...arr) {
   const arrSorted = arr.sort((a, b) => a - b);
   const number1 = arrSorted[Math.floor((arr.length -1) / 2)];
   const number2 = arrSorted[Math.ceil((arr.length -1) / 2)];
   return (number1 + number2) / 2;
 }
 
-function getMin(arr) {
+function getMin(...arr) {
   return Math.min(...arr);
 }
 
-function getMax(arr) {
+function getMax(...arr) {
   return Math.max(...arr);
 }
+
+console.log(calcAverage(4, 90, 27, 38));
+console.log(calcMedian(4, 90, 27, 38));
+console.log(getMin(4, 90, 27, 38));
+console.log(getMax(4, 90, 27, 38));
+
+console.log(calcAverage([4, 90, 27, 38]));
+console.log(calcMedian([4, 90, 27, 38]));
+console.log(getMin([4, 90, 27, 38]));
+console.log(getMax([4, 90, 27, 38]));
